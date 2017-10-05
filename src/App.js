80d75fec53294f6825f5d44bb42fcf1dc88df550
Bill Burgess <wmhburgess@gmail.com>
2017-10-06 10:01:51 +1300

    adds rewards route

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { Route } from 'react-router'
 import LandingPage from './components/LandingPage/LandingPage.js'
 import Register from './components/Register/Register'
 import Home from './components/Home'
+import Rewards from './components/Rewards'
 
 class App extends Component {
   render() {
@@ -13,6 +14,7 @@ class App extends Component {
         <Route exact path='/' component={LandingPage} />
         <Route path='/register' component={Register} />
         <Route path='/home' component={Home} />
+        <Route path='/rewards' component={Rewards} />
       </div>
     );
   }
