2ff23df478afd5c337190dd3baeee3b0a2c21ce3
naeri-kailash <naerikailash@gmail.com>
2017-10-09 09:56:10 +1300

    Reward search functional

--- src/components/RewardCards/Rewards.js
+++ src/components/RewardCards/Rewards.js
@@ -14,7 +14,9 @@ class Rewards extends Component {
     this.handleChange = this.handleChange.bind(this)
 
     this.state = {
-      searchString: ''
+      searchString: '',
+      brandFilter: '',
+      categoryFilter: ''
     }
   }
 
@@ -27,23 +29,56 @@ class Rewards extends Component {
 
   render () {
     const rewardsString = JSON.stringify(rewards)
-    let rewardsToDisplay = this.state.searchString.length > 0
-      ? rewards.filter(reward => {
-        const rewardString = (reward.description +
-          ' ' + reward.brand +
-          ' ' + reward.title +
-          ' ' + reward.description +
-          ' ' + reward.points).toLowerCase()
-        return (
-          rewardString.indexOf(this.state.searchString.toLowerCase()) !== -1
-        )}
-      )
-      : rewards
+    let rewardsToDisplay = rewards.filter(reward => {
+      const rewardString = (reward.description +
+        ' ' + reward.brand +
+        ' ' + reward.title +
+        ' ' + reward.description +
+        ' ' + reward.points).toLowerCase()
+      return (
+        rewardString.indexOf(this.state.searchString.toLowerCase()) !== -1
+      )}
+    )
+    rewardsToDisplay = rewardsToDisplay.filter(reward => {
+      const brand = reward.brand.toLowerCase()
+      return brand.indexOf(this.state.brandFilter.toLowerCase()) !== -1
+    })
+    rewardsToDisplay = rewardsToDisplay.filter(reward => {
+      const category = reward.category.toLowerCase()
+      return category.indexOf(this.state.categoryFilter.toLowerCase()) !== -1
+    })
 
     return (
       <div>
         <div className='container'>
-          <Input className='searchInput' onChange={this.handleChange} type='text' name='searchString' id='searchString' placeholder='Type to search' />
+          <Input
+            className='searchInput'
+            onChange={this.handleChange}
+            type='select'
+            name='brandFilter'
+            id='brandFilter'>
+            <option value=''>Select a brand</option>
+            <option value='vodafone'>Vodafone</option>
+            <option value='spark'>Spark</option>
+            <option value='bp'>BP</option>
+            <option value='paknsave'>Pak n Save</option>
+          </Input>
+          <Input
+            className='searchInput'
+            onChange={this.handleChange}
+            type='select'
+            name='categoryFilter'
+            id='categoryFilter'>
+            <option value=''>Select a category</option>
+            <option value='mobile'>Mobile</option>
+            <option value='food'>Food</option>
+            <option value='fuel'>Fuel</option>
+          </Input>
+          <Input className='searchInput'
+            onChange={this.handleChange}
+            type='text' name='searchString'
+            id='searchString'
+            placeholder='Type to search' />
           Showing {rewardsToDisplay.length} of {rewards.length} rewards
           {rewardsToDisplay.map((reward, key) => {
             return (
