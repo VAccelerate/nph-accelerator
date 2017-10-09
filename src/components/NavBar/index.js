214ee47264e1cfc773b64f42ede0d77f5ae5546f
naeri-kailash <naerikailash@gmail.com>
2017-10-09 15:08:36 +1300

    edit nav links

--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -37,14 +37,14 @@ class NavBar extends Component{
           disableOverlayClick={true}
           customCrossIcon={false}>
           <a style={{position: 'absolute', top: 5, right: 30}} onClick={this.toggleNavbar}><FontAwesomeIcon name='close' /></a>
-          <a id='home' className='menu-item' href='/' onClick={this.toggleNavbar}>Home</a>
-          <a id='babyAndToddle' className='menu-item' href='/about' onClick={this.toggleNavbar}>Baby & Toddler</a>
-          <a id='pregnancy' className='menu-item' href='/pregnancy' onClick={this.toggleNavbar}>Pregnancy</a>
+          <a id='home' className='menu-item' href='/home' onClick={this.toggleNavbar}>Home</a>
+          <a id='babyAndToddler' className='menu-item' href='#' onClick={this.toggleNavbar}>Baby & Toddler</a>
+          <a id='pregnancy' className='menu-item' href='#' onClick={this.toggleNavbar}>Pregnancy</a>
           <a id='rewards' className='menu-item' href='/rewards' onClick={this.toggleNavbar}>Rewards</a>
-          <a id='calendar' className='menu-item' href='/calendar' onClick={this.toggleNavbar}>Calendar</a>
-          <a id='supportAndHelp' className='menu-item' href='/supportAndHelp' onClick={this.toggleNavbar}>Support & Help</a>
-          <a id='contact' className='menu-item' href='/contact' onClick={this.toggleNavbar}>Contact</a>
-          <a onClick={ this.showSettings } className='menu-item--small' href='' onClick={this.toggleNavbar}>Settings</a>
+          <a id='calendar' className='menu-item' href='#' onClick={this.toggleNavbar}>Calendar</a>
+          <a id='supportAndHelp' className='menu-item' href='#' onClick={this.toggleNavbar}>Support & Help</a>
+          <a id='myPoints' className='menu-item' href='/points' onClick={this.toggleNavbar}>My Rewards</a>
+          <a id='contact' className='menu-item' href='#' onClick={this.toggleNavbar}>Contact</a>
         </Menu>
         <Navbar color='faded' light>
           <NavbarToggler onClick={this.toggleNavbar} style={{border: 'none'}} />
