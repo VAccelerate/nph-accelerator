1b8f8e9fd61d08aee485369a89ada37a8c419791
naeri-kailash <naerikailash@gmail.com>
2017-10-11 15:45:43 +1300

    content page for finding your midwife created

---
+++ src/components/SectionPage/ArticlePage.js
@@ -0,0 +1,86 @@
+import React, { Component } from 'react'
+import {
+  Container,
+  Row,
+  Col,
+  Card,
+  CardText
+} from 'reactstrap'
+import { Link } from 'react-router-dom'
+
+import './articlePage.css'
+import NavBar from '../NavBar'
+import Footer from '../Footer'
+import yourPregnancyArticles from './yourPregnancyArticles'
+import supportAndHelpArticles from './supportAndHelpArticles'
+import babyAndToddlerArticles from './babyAndToddlerArticles'
+import backImg from '../../img/angle/angle-left.svg'
+
+class ArticlePage extends Component {
+  render () {
+    const section = window.location.pathname
+    const splitUrl = section.split('/')
+    const link = splitUrl[1]
+    const id = splitUrl[2]
+    const idPlusOne = Number(id) + 1
+    let data = []
+    if (link === 'your-pregnancy') {
+      data = yourPregnancyArticles
+    } else if (link === 'baby-and-toddler') {
+      data = babyAndToddlerArticles
+    } else if (link === 'support-and-help') {
+      data = supportAndHelpArticles
+    }
+    const relevantData = data[id]
+    const { title, content, youtubeUrl, img, relatedSection, readMore } = relevantData
+    return (
+      <div>
+        <NavBar />
+        <Link to={`/${link}`}>
+          <Row>
+            <Col xs='2'>
+              <img className='backImg' src={backImg} alt='' />
+            </Col>
+            <Col className='back' xs='10'>
+              Back to {relatedSection}
+            </Col>
+          </Row>
+        </Link>
+        <img className='mainImg' src={img} alt='' />
+        <Container>
+          <h4 className='articleTitle'>{title}</h4>
+          {content}
+          {
+            id < data.length
+            ? <Link to={`/your-pregnancy/${idPlusOne}`}><Card className='nextButton'><CardText className='nextText'>Next article > </CardText></Card></Link>
+            : null
+          }
+          <hr className='hr' />
+          {
+            readMore
+            ? <div>
+              <h4 className='readMore'>Read more</h4>
+              <ul>
+                {
+                  readMore.map((item, key) => {
+                    return (
+                      <Link to={item.link}>
+                        <li className='li'>
+                          {item.title}
+                        </li>
+                      </Link>
+                    )
+                  })
+                }
+              </ul>
+            </div>
+            : null
+          }
+        </Container>
+        <Footer />
+      </div>
+    )
+  }
+}
+
+export default ArticlePage
--- src/components/SectionPage/yourPregnancyArticles.js
+++ src/components/SectionPage/yourPregnancyArticles.js
@@ -1,26 +1,71 @@
+import React from 'react'
+
+import gettingAMidwifeImg from '../../img/ArticlePages/gettingAMidwife.png'
 
 const yourPregnancyArticles = [
   {
     id: 0,
-    relatedSection: 'your pregnancy',
-    title: 'Finding a Midwife',
+    relatedSection: 'Your Pregnancy',
+    title: 'Getting a Midwife',
     description: 'Get help sorting out a midwife',
-    content: '',
+    content: (
+      <div>
+        <div className='contentStyle'>
+          As soon as you know you are pregnant, you need to contact a
+          midwife and sent up an appointment.
+        </div>
+        <div className='contentStyle last'>
+          There are two midwives available on the east coast. You can contact
+          either <span className='nameStyle'>Corrina Parata</span> at Ngati Porou Hauora or <span className='nameStyle'>Liz Tamepo</span> at Te
+          Korahi Whanautanga Midwifery Services.
+        </div>
+        <h4 className='midwifeName'>Liz Tamepo</h4>
+        <ul className='midwifeLocations'>
+          <li>Tuesday: Te Araroa</li>
+          <li>Wednesday: Ruatoria</li>
+          <li>Thursday: Puhi Kaiti</li>
+        </ul>
+        <ul className='midwifeContact'>
+          <li><span className='midwifeContactType'>Phone: </span>06 864 7788</li>
+          <li><span className='midwifeContactType'>Mobile: </span><span>027 237 8467</span></li>
+          <li><span className='midwifeContactType'>Email: </span><span className='midwifeEmail'>lizzie.andre@xtra.co.nz</span></li>
+        </ul>
+        <h4 className='midwifeName'>Corrina Parata</h4>
+        <ul className='midwifeLocations'>
+          <li>Ngati Porou Hauora</li>
+        </ul>
+        <ul className='midwifeContact'>
+          <li><span className='midwifeContactType'>Phone: </span>06 864 7788</li>
+          <li><span className='midwifeContactType'>Mobile: </span><span>027 237 8467</span></li>
+          <li><span className='midwifeContactType'>Email: </span><span className='midwifeEmail'>lizzie.andre@xtra.co.nz</span></li>
+        </ul>
+      </div>
+    ),
     youtubeUrl: '',
-    img: ''
+    img: gettingAMidwifeImg,
+    readMore: [
+      {
+        title: 'Food & Nutrition',
+        link: '/your-pregnancy'
+      },
+      {
+        title: 'Exercise & Fitness',
+        link: '/your-pregnancy'
+      }
+    ]
   },
   {
     id: 1,
-    relatedSection: 'your pregnancy',
+    relatedSection: 'Your Pregnancy',
     title: 'Food & Nutrition',
     description: 'Help with your diet',
-    content: '',
+    content: 'hi',
     youtubeUrl: '',
     img: ''
   },
   {
     id: 2,
-    relatedSection: 'your pregnancy',
+    relatedSection: 'Your Pregnancy',
     title: 'Exercise & Fitness',
     description: 'View claimed rewards',
     content: '',
@@ -29,7 +74,7 @@ const yourPregnancyArticles = [
   },
   {
     id: 2,
-    relatedSection: 'your pregnancy',
+    relatedSection: 'Your Pregnancy',
     title: 'Common Symptoms',
     description: 'What to look for',
     content: '',
@@ -38,7 +83,7 @@ const yourPregnancyArticles = [
   },
   {
     id: 2,
-    relatedSection: 'your pregnancy',
+    relatedSection: 'Your Pregnancy',
     title: 'Smoking, Drugs & Alcohol',
     description: 'Advise on what you can do',
     content: '',
