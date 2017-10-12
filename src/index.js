61d1cf9a4ae94c5a1d5b116c5a889a47e88b8fc8
Jamie Bowley <jamie.bowley@gmail.com>
2017-10-12 13:58:21 +1300

    Remove redux devtools

--- src/index.js
+++ src/index.js
@@ -21,8 +21,8 @@ let store = null
 
 if (process.env.NODE_ENV === 'development') {
   store = createStore(reducers, undefined, compose(
-    applyMiddleware(thunkMiddleware),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    applyMiddleware(thunkMiddleware)
+    // window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
   ))
 } else {
   store = createStore(reducers, undefined, compose(
